d34a8b5bd10163c5b253735a9c6b185bce453db6
Jammin <benjaminsepada111@gmail.com>
2026-03-17 22:16:21 +0800

    Fix routes and remove all auth middleware

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,7 @@
 use App\Http\Controllers\ActivityController;
 use Illuminate\Support\Facades\Route;
 
-// No authentication required for these routes
-Route::withoutMiddleware(['auth'])->group(function () {
-    
-    // Receive a ping from the Python tracker
-    Route::post('/activity', [ActivityController::class, 'store']);
-
-    // Get all users current status (Flutter dashboard)
-    Route::get('/users/status', [ActivityController::class, 'allStatus']);
-
-    // Get one user logs
-    Route::get('/users/{id}/daily',  [ActivityController::class, 'daily']);
-    Route::get('/users/{id}/weekly', [ActivityController::class, 'weekly']);
-    
-});
+Route::post('/activity', [ActivityController::class, 'store']);
+Route::get('/users/status', [ActivityController::class, 'allStatus']);
+Route::get('/users/{id}/daily', [ActivityController::class, 'daily']);
+Route::get('/users/{id}/weekly', [ActivityController::class, 'weekly']);
